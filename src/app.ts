c6d7ed0b8bbda936bba7227caa77839a1a791e5b
odan <odan3240@gmail.com>
2020-05-02 19:44:02 +0900

    add express

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,15 @@
+import express from "express";
+
+const app = express();
+
 async function main(): Promise<void> {
   console.log("poyo");
+
+  const port = 3000;
+
+  app.listen(port, () => {
+    console.log(`http://localhost:${port}`);
+  });
 }
 
 main();
